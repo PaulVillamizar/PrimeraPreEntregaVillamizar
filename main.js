1d8f680cb6ed42eb6d5eff297aa8a08db3df3762
Paul Villamizar <142462025+PaulVillamizar@users.noreply.github.com>
2023-12-13 19:41:34 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,20 @@
+function calcularCostoTotal() {
+    const cantidadDeProductos = parseInt(
+        prompt("Ingrese la cantidad de productos:")
+    );
+    let costoTotal = 0;
+    for (let i = 1; i <= cantidadDeProductos; i++) {
+        const precioProducto = parseFloat(
+            prompt(`Ingrese el precio del producto ${i}:`)
+        );
+        if (!isNaN(precioProducto) && precioProducto > 0) {
+            costoTotal += precioProducto;
+        } else {
+            alert("El precio ingresado es inválido. Inténtelo nuevamente.");
+            i--;
+        }
+    }
+    alert(`El costo total de los productos es: $${costoTotal.toFixed(2)}`);
+}
+
+calcularCostoTotal();
